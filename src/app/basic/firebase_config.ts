49b4e08a6dda1d4294d79e6953d52e3116aecd3d
WeakenedPlayer <weakenedplayer@gmail.com>
2017-01-08 20:37:49 +0900

    ignore設定を修正

--- src/app/basic/firebase_config.ts
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-  apiKey: '<your-key>',
-  authDomain: '<your-project-authdomain>',
-  databaseURL: '<your-database-URL>',
-  storageBucket: '<your-storage-bucket>',
-  messagingSenderId: '<your-messaging-sender-id>'
-};
