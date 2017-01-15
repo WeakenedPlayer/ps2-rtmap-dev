5ef19e2f81210d758fa9c4095e284a5dabf50313
WeakenedPlayer <weakenedplayer@gmail.com>
2017-01-15 16:42:27 +0900

    定数サービスに移行

--- src/app/basic/leaflet-test/leaflet-test.component.ts
+++ src/app/basic/leaflet-test/leaflet-test.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Renderer, ElementRef, Input } from '@angular/core';
 import * as Leaflet from 'leaflet';
 import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
-// import { ConstantsService } from '../../services/constants/constants.service';
+import { ConstantsService } from '../../services/constants/constants.service';
 
 @Component({
   selector: 'app-leaflet-test',
@@ -11,30 +11,21 @@ import { AngularFire, FirebaseObjectObservable, FirebaseListObservable } from 'a
 
 export class LeafletTestComponent implements OnInit {
   // map related options
-  mapOption: Leaflet.MapOptions = {
-    attributionControl: false,
-    crs: Leaflet.CRS.Simple,
-    center: [ -128, 128 ],
-    zoom: 1
-  };
+  mapOption: Leaflet.MapOptions;
   @Input() tileUrl: string = '';
-  tileOption: Leaflet.TileLayerOptions = {
-    tileSize: 256,
-    minZoom: 1,
-    maxZoom: 7,
-    maxNativeZoom: 5,
-    noWrap: true
-  };
+  tileOption: Leaflet.TileLayerOptions;
   af: AngularFire;
   markerOption: Leaflet.MarkerOptions = { draggable: true };
   markerObserver: FirebaseListObservable<any>;
   tmpLatLng: Leaflet.LatLng = Leaflet.latLng( [ 0, 0 ]);
 
-  constructor( af: AngularFire) {
+  constructor( af: AngularFire, cs: ConstantsService ) {
     this.af = af;
     this.markerObserver = this.af.database.list('/loc');
-    this.markerObserver.subscribe( snapshot => {
-    });
+    this.markerObserver.subscribe( snapshot => {});
+
+    this.mapOption = ConstantsService.MapOption;
+    this.tileOption = ConstantsService.TileOption;
   }
 
   ngOnInit() {
