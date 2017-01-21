cdffc13430fecce602817ffce62130a5ddbd720f
WeakenedPlayer <weakenedplayer@gmail.com>
2017-01-22 00:03:08 +0900

    途中保存

--- src/app/basic/rbac/rbac.service.ts
+++ src/app/basic/rbac/rbac.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
 import { AngularFire , FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
-
 import { Subscription } from 'rxjs';
 
 const URL_BASE = '/ttt';
+
+export class Permission {
+  name: string;
+  description: string;
+
+  constructor( name: string, descritpion: string ) {
+    this.name = name;
+    this.description = descritpion;
+  }
+}
+
+export class Role {
+  name: string;
+  description: string;
+
+  constructor( name: string, descritpion: string ) {
+    this.name = name;
+    this.description = descritpion;
+  }
+}
 
 @Injectable()
 export class RbacService {
@@ -11,65 +30,76 @@ export class RbacService {
 
   // FirebaseのURL
   static getRolesUrl(): string { return URL_BASE + '/roles'; }
+  static getRoleUrl( roleKey: string ): string { return RbacService.getRolesUrl() + '/' + roleKey; }
   static getPermissionsUrl(): string { return URL_BASE + '/permissions'; }
+  static getPermissionUrl( permissionKey: string ): string { return RbacService.getPermissionsUrl() + '/' + permissionKey; }
   static getAssignmentUrl(): string { return URL_BASE + '/assign'; }
-  static getAllRolePermissionsUrl( roleKey: string ): string {
-    return RbacService.getAssignmentUrl() + '/' + roleKey;
-  }
-  static getRolesPermissionUrl( roleKey: string, permissionKey: string ): string {
-    return RbacService.getAllRolePermissionsUrl( roleKey ) + '/' + permissionKey;
-  }
+  static getAllRolePermissionsUrl( roleKey: string ): string { return RbacService.getAssignmentUrl() + '/' + roleKey; }
+  static getRolesPermissionUrl( roleKey: string, permissionKey: string ): string { return RbacService.getAllRolePermissionsUrl( roleKey ) + '/' + permissionKey; }
 
-  constructor( af: AngularFire ) {
-    this.af = af;
-  }
+  constructor( af: AngularFire ) { this.af = af; }
 
-  // simple get
-  getRoles(): FirebaseListObservable<any> {
-    return this.af.database.list( RbacService.getRolesUrl() );
-  }
+  // Roleの取得
+  getRoles(): FirebaseListObservable<any> { return this.af.database.list( RbacService.getRolesUrl() ); }
+  getRole( roleKey: string ): FirebaseObjectObservable<any> { return this.af.database.object( RbacService.getRoleUrl( roleKey ) ); }
 
-  getPermissions(): FirebaseListObservable<any> {
-    return this.af.database.list( RbacService.getPermissionsUrl() );
-  }
+  // Permissionの取得
+  getPermissions(): FirebaseListObservable<any> { return this.af.database.list( RbacService.getPermissionsUrl() ); }
+  getPermission( permissionKey: string ): FirebaseObjectObservable<any> { return this.af.database.object( RbacService.getPermissionUrl( permissionKey ) ); }
 
-  // Roleに付与されたPermission一覧
-  getAllRolePermissions( roleKey: string ): FirebaseObjectObservable<any> {
-    return this.af.database.object( RbacService.getAllRolePermissionsUrl( roleKey ) );
-  }
+  // assignmentの取得(RoleへのPermission割り当て)
+  getAssignments(): FirebaseListObservable<any> { return this.af.database.list( RbacService.getAssignmentUrl() + '/assign' ); }
+  getRolePermissions( roleKey: string ): FirebaseObjectObservable<any> { return this.af.database.object( RbacService.getAllRolePermissionsUrl( roleKey ) ); }
+  getRolePermission( roleKey: string, permissionKey: string ): FirebaseObjectObservable<any> { return this.af.database.object( RbacService.getRolesPermissionUrl( roleKey, permissionKey ) ); }
 
-  // RoleのPermission
-  getRolePermission( roleKey: string, permissionKey: string ): FirebaseObjectObservable<any> {
-    return this.af.database.object( RbacService.getRolesPermissionUrl( roleKey, permissionKey ) );
-  }
-
-  // RoleのPermissionの許可/禁止を設定する
-  setPermission( roleKey: string, permissionKey: string, permission) {
-    this.getRolePermission( roleKey, permissionKey ).set( permission );
-  }
-
-  // permission を assignment から一括削除する
-  deletePermissionFromAllAssignments( permissionKey: string ) {
-    let assignments = this.af.database.list( URL_BASE + '/assign' );
-    let assignmentsSubscriber = assignments.subscribe( assignmentsSnapshot => {
-      assignmentsSubscriber.unsubscribe();
-
-      for ( let assignment of assignmentsSnapshot ) {
-        if ( assignment[ permissionKey ] !== undefined ) {
-          // 暫定
-          this.getRolePermission( assignment.$key, permissionKey ).remove();
-        }
-      }
-   });
-  }
-
-  // 全ての role に 指定した permission を追加する
-  addPermissionToAllAssignments( permissionKey: string, initValue: boolean ) {
+  // assignmentの設定
+  assignPermissionToRole( permissionKey: string, roleKey: string, permission: boolean ) { this.getRolePermission( roleKey, permissionKey ).set( permission ); }
+  assignPermissionToAllRoles( permissionKey: string, initValue: boolean ) {
     let rolesSubscriber = this.getRoles().subscribe( rolesSnapshot => {
       rolesSubscriber.unsubscribe();
       for ( let role of rolesSnapshot ) {
-        this.setPermission( role.$key, permissionKey, initValue );
+        this.assignPermissionToRole( permissionKey, role.$key, initValue );
       }
     });
+  }
+
+  // role を削除する (連動してassignment からも削除する)
+  deleteRole( roleKey: string ) {
+    // 瞬間的な不整合は気にしない(管理者しか操作しないので)
+    this.getRolePermissions( roleKey ).remove();
+    this.getRole( roleKey ).remove();
+  }
+
+  // permission を削除する (連動して assignment からも削除する)
+  deletePermission( permissionKey: string ) {
+    let assignmentsSubscriber = this.getAssignments().subscribe( assignmentsSnapshot => {
+      // 1回だけデータを取得するだけなので unsubscribe しておく
+      assignmentsSubscriber.unsubscribe();
+
+      for ( let role of assignmentsSnapshot ) {
+        // assign/<role>/permission を削除する
+        if ( role[ permissionKey ] !== undefined ) {
+          this.getRolePermission( role.$key, permissionKey ).remove();
+        }
+      }
+      // 瞬間的な不整合は気にしない(管理者しか操作しないので)
+      this.getPermission( permissionKey ).remove();
+   });
+  }
+
+  // Permissionの生成
+  createPermission( name: string, description: string ): firebase.database.ThenableReference {
+    return this.getPermissions().push( new Permission( name, description ) );
+  }
+  updatePermission( permissionKey: string, param: any ) {
+    return this.getPermission( permissionKey ).update( param as Permission );
+  }
+
+  // Permissionの生成
+  createRole( name: string, description: string ): firebase.database.ThenableReference {
+    return this.getRoles().push( new Role( name, description ) );
+  }
+  updateRole( permissionKey: string, param: any ) {
+    return this.getRole( permissionKey ).update( param as Role );
   }
 }
